7c9201ad51cecfa46e53c9fba0e8841fe6cd534f
Diogo Menezes <diogo444@hotmail.com>
2020-12-22 16:38:37 +0000

    remove initial point

--- userPosition.js
+++ userPosition.js
@@ -4,7 +4,7 @@ var lng = -16.354469
 navigator.geolocation.getCurrentPosition(function success(position) {
   // lat = position.coords.latitude
   // lng = position.coords.longitude
-  let sceneEl = document.querySelector('a-scene')
+  // let sceneEl = document.querySelector('a-scene')
 
   // Calculate new lat and lng method
   // new_latitude = latitude + (dy / r_earth) * (180 / pi)
@@ -13,20 +13,20 @@ navigator.geolocation.getCurrentPosition(function success(position) {
   var newLat = lat + (-0.0009 / 111) * (180 / 3.14159265)
   var newLng = lng + ((-0.0009 / 111) * (180 / 3.14159265)) / Math.cos((lat * 3.14159265) / 180)
 
-  let markerEl = document.createElement('a-link')
+  // let markerEl = document.createElement('a-link')
 
-  markerEl.setAttribute('color', 'red')
+  // markerEl.setAttribute('color', 'red')
 
-  markerEl.setAttribute('title', 'Ponto 1')
-  markerEl.setAttribute('scale', '3 3 3')
+  // markerEl.setAttribute('title', 'Ponto 1')
+  // markerEl.setAttribute('scale', '3 3 3')
 
-  markerEl.addEventListener('loaded', () => {
-    window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-  })
+  // markerEl.addEventListener('loaded', () => {
+  //   window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+  // })
 
-  markerEl.setAttribute('gps-projected-entity-place', `latitude: ${newLat};longitude: ${newLng}`)
+  // markerEl.setAttribute('gps-projected-entity-place', `latitude: ${newLat};longitude: ${newLng}`)
 
-  sceneEl.appendChild(markerEl)
+  // sceneEl.appendChild(markerEl)
 
   let instructionsEl = document.getElementById('info')
   instructionsEl.innerHTML = `lat: ${newLat.toFixed(4)}; lng:${newLng.toFixed(4)}`
